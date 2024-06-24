bcb20799568ec3fb67efaf2015001f948e533d78
n-mangini <nicolas.mangini@ing.austral.edu.ar>
2024-06-24 00:57:13 -0300

    fix generate-https-certificates.sh

--- generate-https-certificates.sh
+++ generate-https-certificates.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # run this script to generate lets encrypt certificates only once
 
-# Load environment variables from the .env file
+# Load the DOMAIN_NAME environment variable from the .env file
 if [ -f .env ]; then
-  export $(cat .env | xargs)
+  DOMAIN_NAME=$(grep '^DOMAIN_NAME=' .env | cut -d '=' -f2-)
+  # Remove surrounding quotes if present
+  DOMAIN_NAME=$(echo $DOMAIN_NAME | sed -e 's/^"//' -e 's/"$//')
+  DOMAIN_NAME=$(echo $DOMAIN_NAME | sed -e "s/^'//" -e "s/'$//")
 fi
 
 # Check if DOMAIN_NAME is set
@@ -12,4 +15,4 @@ if [ -z "$DOMAIN_NAME" ]; then
   exit 1
 fi
 
-docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ -d $DOMAIN_NAME
+docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ -d $DOMAIN_NAME
